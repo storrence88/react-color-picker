d480c10268203dc9c1f79b577e896d5128faba1f
Steven Torrence <storrence88@gmail.com>
2020-06-13 15:27:59 -0500

    Add react-router-dom dependency and add basic Switch and Route components to App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
@@ -6,9 +7,13 @@ import { generatePalette } from './colorHelpers';
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path='/' render={() => <h1>Palette List goes here!</h1>} />
+        <Route exact path='/palette/:id' render={() => <h1>Individual Palette goes here!</h1>} />
+      </Switch>
+      // <div className='App'>
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
     );
   }
 }
