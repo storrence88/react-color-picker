e384945b2cedec18aa79283db7393dc134f4245f
Steven Torrence <storrence88@gmail.com>
2020-06-14 20:56:08 -0500

    Use history.push to avoid using Link components around interactive content

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -34,6 +34,10 @@ const styles = {
 };
 
 class PaletteList extends Component {
+  goToPalette = id => {
+    this.props.history.push(`/palette/${id}`);
+  };
+
   render() {
     const { palettes, classes } = this.props;
     return (
@@ -43,7 +47,9 @@ class PaletteList extends Component {
             <h1>React Colors!</h1>
           </nav>
           <div className={classes.palettes}>
-            {palettes.map(palette => <MiniPalette {...palette} />)}
+            {palettes.map(palette => (
+              <MiniPalette {...palette} handleClick={() => this.goToPalette(palette.id)} />
+            ))}
           </div>
         </div>
       </div>
