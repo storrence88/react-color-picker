1f64ac3762bfb7a898bc438aec148b7d3f3a75bf
Steven Torrence <storrence88@gmail.com>
2020-06-17 21:34:22 -0500

    Add navbar and footer to SingleColorPalette component

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,7 +15,7 @@ export default class Navbar extends Component {
     this.state = { format: 'hex', open: false };
   }
 
-  handleFormatChange = event => {
+  handleFormatChange = (event) => {
     this.setState({ format: event.target.value, open: true });
     this.props.handleChange(event.target.value);
   };
@@ -25,25 +25,27 @@ export default class Navbar extends Component {
   };
 
   render() {
-    const { level, changeLevel } = this.props;
+    const { level, changeLevel, showingAllColors } = this.props;
     const { format } = this.state;
     return (
       <header className='Navbar'>
         <div className='logo'>
           <Link to='/'>Steve's React Color Picker</Link>
         </div>
-        <div className='slider-container'>
-          <span>Level: {level}</span>
-          <div className='slider'>
-            <Slider
-              defaultValue={level}
-              min={100}
-              max={900}
-              step={100}
-              onAfterChange={changeLevel}
-            />
+        {showingAllColors && (
+          <div className='slider-container'>
+            <span>Level: {level}</span>
+            <div className='slider'>
+              <Slider
+                defaultValue={level}
+                min={100}
+                max={900}
+                step={100}
+                onAfterChange={changeLevel}
+              />
+            </div>
           </div>
-        </div>
+        )}
         <div className='select-container'>
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value='hex'>HEX - #ffffff</MenuItem>
