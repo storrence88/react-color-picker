fb666c4837cb64f56feb48d0c87f3a355f903a41
Steven Torrence <storrence88@gmail.com>
2020-06-25 22:02:18 -0500

    Add DraggableColorBox component

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -12,6 +12,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Button from '@material-ui/core/Button';
+import DraggableColorBox from './DraggableColorBox';
 import { ChromePicker } from 'react-color';
 
 const drawerWidth = 400;
@@ -57,6 +58,7 @@ const styles = (theme) => ({
   },
   content: {
     flexGrow: 1,
+    height: 'calc(100vh - 64px)',
     padding: theme.spacing.unit * 3,
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
@@ -165,9 +167,8 @@ class NewPaletteForm extends Component {
           })}
         >
           <div className={classes.drawerHeader} />
-          <ul>
-            {this.state.colors.map((color) => <li style={{ backgroundColor: color }}>{color}</li>)}
-          </ul>
+
+          {this.state.colors.map((color) => <DraggableColorBox color={color} />)}
         </main>
       </div>
     );
